990a8fd6e63f5628fbb6a2fe4d3e8f9c0a32af1b
calcantara <claudio.ta@gmail.com>
2020-04-03 16:56:15 -0300

    Alterei o metodo u.rb

--- lib/reverse_markdown/converters/u.rb
+++ lib/reverse_markdown/converters/u.rb
@@ -2,8 +2,8 @@ module ReverseMarkdown
   module Converters
     class U < Base
       def convert(node, state = {})
-        content = treat_children(node, state.merge(already_u: true))
-        if content.strip.empty? || state[:already_u]
+        content = treat_children(node, state.merge(already_underline: true))
+        if content.strip.empty? || state[:already_underline]
           content
         else
           "#{content[/^\s*/]}__#{content.strip}__#{content[/\s*$/]}"
